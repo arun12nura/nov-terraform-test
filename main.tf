f2b566761c0297f809afdb9b967b840a53e13e9e
arun12nura <55918280+arun12nura@users.noreply.github.com>
2022-11-11 10:16:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ variable "hw" {
 
 variable "name" {
   type = string
-  default = "gagan-server1"
+  default = "arun-server1"
   description = "Enter your server name"
 }
 
@@ -26,8 +26,8 @@ resource "aws_instance" "myawsserver" {
 
   tags = {
     Name = var.name
-    Env = "test"
-    Owner = "Gagandeep"
+    Env = "DEV"
+    Owner = "ARUN"
   }
 }
 
